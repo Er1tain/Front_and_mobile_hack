f610a9b44f9ac613162db4812f0ac9f1066164db
unknown <sanyapridava@mail.ru>
2024-08-03 07:12:05 +0300

    Стилизован IonPage

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { IonPage } from "@ionic/react";
+import styled from 'styled-components';
+
+const Page = styled(IonPage)`
+    width: 100%;
+    height: 100%;
+    background-color: #669DFB;
+`
 
 export default function Welcome() {
     return (
-        <IonPage>
+        <Page>
             
-        </IonPage>
+        </Page>
     )
 }
